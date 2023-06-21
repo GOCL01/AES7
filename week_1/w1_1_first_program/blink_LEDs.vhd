0fb0fd3a03454ca320d8c59f8780aa6de30d107a
GOCL01 <95654042+GOCL01@users.noreply.github.com>
2023-06-21 11:43:38 +0200

    Update blink_LEDs.vhd

--- week_1/w1_1_first_program/blink_LEDs.vhd
+++ week_1/w1_1_first_program/blink_LEDs.vhd
@@ -7,21 +7,12 @@
 -- Module Name: blink_LEDs - Behavioral
 -- Project Name: blink_LEDs
 -- Target Devices: zybo z7-10
--- Description: Simple design that control LED's based
---              buttons 
+-- Description: Simple design that controls LED's based
+--              inout buttons 
 ----------------------------------------------------------------------------------
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity blink_LEDs is
 Port ( BT : in std_logic_vector(3 downto 0); -- input 
@@ -33,9 +24,9 @@ architecture Behavioral of blink_LEDs is
 
 begin
 
-LED(0) <=  BT(0);
-LED(1) <=  BT(1);
-LED(2) <=  BT(2);
-LED(3) <=  BT(3);
+LED(0) <=  BT(0); -- when button 1 is pressed, LED 1 is on
+LED(1) <=  BT(1); -- when button 2 is pressed, LED 2 is on
+LED(2) <=  BT(2); -- when button 3 is pressed, LED 3 is on
+LED(3) <=  BT(3); -- when button 4 is pressed, LED 4 is on
 
 end Behavioral;
